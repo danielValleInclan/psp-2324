2ba725918bd5d5489302628598af6e83f1d2ef11
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2023-10-10 10:12:39 +0200

    Mejorados ejemplos

--- ut1/LanzarProcesos/src/Ejemplo1.java
+++ ut1/LanzarProcesos/src/Ejemplo1.java
@@ -1,14 +1,7 @@
+import java.io.IOException;
 
 public class Ejemplo1 {
-    public static void main(String[] args) {
-        Runtime r = Runtime.getRuntime();
-        String command = "gedit";
-        Process p = null;
-        try {
-            p = r.exec(command);
-        } catch (Exception e){
-            System.out.println("Error en: " + command);
-            e.printStackTrace();
-        }
+    public static void main(String[] args) throws IOException {
+        Process process = new ProcessBuilder("gedit").start();
     }
 }
--- ut1/LanzarProcesos/src/Ejemplo2.java
+++ ut1/LanzarProcesos/src/Ejemplo2.java
@@ -1,29 +1,27 @@
 import java.io.BufferedReader;
+import java.io.IOException;
 import java.io.InputStream;
 import java.io.InputStreamReader;
 
 public class Ejemplo2 {
-    public static void main(String[] args) {
-        Runtime r = Runtime.getRuntime();
-        String command = "ls";
-        Process p = null;
+    public static void main(String[] args) throws IOException {
+
+        Process process = new ProcessBuilder("ls", "-l").start();
         try {
-            p = r.exec(command);
-            InputStream is = p.getInputStream();
-            BufferedReader br = new BufferedReader(new InputStreamReader(is));
-            String line;
-            while ((line = br.readLine()) != null) System.out.println(line);
-            br.close();
-        } catch (Exception e){
-            System.out.println("Error en: " + command);
-            e.printStackTrace();
-        }
-        //COMPROBACIÓN DE ERROR - 0 bien - 1 mal
-        int exitVal;
-        try {
-            exitVal = p.waitFor();
-            System.out.println("Valor de salida: " + exitVal);
-        } catch (Exception e) {
+            InputStream inputStream = process.getInputStream();
+            BufferedReader reader = new BufferedReader(new InputStreamReader(inputStream));
+
+            String linea;
+            while ((linea = reader.readLine()) != null) {
+                // Imprimir cada línea del resultado (nombre de archivo o directorio)
+                System.out.println(linea);
+            }
+
+            // Esperar a que el proceso termine
+            int exitCode = process.waitFor();
+            System.out.println("El proceso ha terminado con código de salida: " + exitCode);
+
+        } catch (IOException | InterruptedException e) {
             e.printStackTrace();
         }
     }
