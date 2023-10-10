0c7fd4d2170e46000bb7b941d44aed4be6ed9ac4
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2023-10-10 10:26:00 +0200

    Plantilla pegada a ut1/LanzarProcesos

--- ut1/LanzarProcesos/src/ejemplos/Ejemplo1.java
+++ ut1/LanzarProcesos/src/ejemplos/Ejemplo1.java
@@ -1,3 +1,5 @@
+package ejemplos;
+
 import java.io.IOException;
 
 public class Ejemplo1 {
--- ut1/LanzarProcesos/src/ejemplos/Ejemplo2.java
+++ ut1/LanzarProcesos/src/ejemplos/Ejemplo2.java
@@ -1,3 +1,5 @@
+package ejemplos;
+
 import java.io.BufferedReader;
 import java.io.IOException;
 import java.io.InputStream;
