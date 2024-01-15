2f72f5fcb415b37523609356d68282c9364be287
danielrodriguez <daniel.rodriguezvilches@iesvalleinclan.es>
2024-01-15 13:25:45 +0100

    cliente mod

--- ut3/objetos/src/actividades/cta/act3/Cliente.java
+++ ut3/objetos/src/actividades/cta/act3/Cliente.java
@@ -1,13 +1,11 @@
 package actividades.cta.act3;
 
-import java.io.ByteArrayOutputStream;
-import java.io.IOException;
-import java.io.ObjectOutputStream;
+import java.io.*;
 import java.net.*;
 import java.util.Scanner;
 
 public class Cliente {
-    public static void main(String[] args) throws IOException {
+    public static void main(String[] args) throws IOException, ClassNotFoundException {
         Scanner sc = new Scanner(System.in);
         InetAddress destino = InetAddress.getLocalHost();
         int port = 12345;
@@ -23,6 +21,20 @@ public class Cliente {
             DatagramSocket socket = new DatagramSocket(34567); // Puerto local
 
             socket.send(envio);
+
+            byte[] recibidos = new byte[1024];
+            DatagramPacket paqRecibido = new DatagramPacket(recibidos, recibidos.length);
+            socket.receive(paqRecibido);//recibo datagrama
+            ByteArrayInputStream bais = new ByteArrayInputStream(recibidos);
+            ObjectInputStream in = new ObjectInputStream(bais);
+            Alumno alumno = (Alumno) in.readObject();
+
+            in.close();
+
+            System.out.println("Recibido -> \n" +
+                    "Nombre: " + alumno.getNombre() +
+                    "Nota: " + alumno.getNota() +
+                    "Curso: " + alumno.getCurso());
             socket.close();
         } while (!idalumno.equals("*"));
     }
